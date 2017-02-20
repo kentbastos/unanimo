e83da8d1d9cf1bf2bf284619a2912cbe6713f517
FRANCK <david.franck@cbp-prevoyance.net>
2017-02-20 18:18:51 +0100

    suite groovy, template en v1

--- version_groovy/unanimo.groovy
+++ version_groovy/unanimo.groovy
@@ -17,50 +17,34 @@ inputData.players.each { player ->
 		allWords = allWords.collect { 
 			Normalizer.normalize(it.toUpperCase(), Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
 		}
-		currentPlayer.proposalsPerDay.put(dayIndex.toInteger(), allWords.collect { new Word(value: it) })
+		currentPlayer.proposalsPerDay.put(dayIndex.toInteger(), allWords.collect { new Word(value: it.trim()) })
 		maxDay = Math.max(maxDay, dayIndex.toInteger())
 	}
 	players << currentPlayer
 }
 
-println "lecture des données ok"
+println "> lecture des données ok"
+
+checkInput(players)
+
+// TODO check des doublons et de la taille de données
 
 // calcul de la liste des mots et des points associés
 def pointsForWordsOfDays = [:] // clé = jour / valeur = liste de Word
 (0..maxDay).each { day -> pointsForWordsOfDays.put(day, computePointsForWordsOfDay(day, players)) }
 
-println "calcul des points par mot pour chaque jour ok"
+println "> calcul des points par mot pour chaque jour ok"
 
 // affectation des points des joueurs
-players.each { player ->
-	player.proposalsPerDay.each { day, playerWords ->
-		def wordsOfDay = pointsForWordsOfDays[day]
-		def totalForDay = 0
-		playerWords.each { playerWord ->
-			def wordOfDay = wordsOfDay.find { it.value == playerWord.value }
-			if(wordOfDay){
-				playerWord.points  = wordOfDay.points
-				totalForDay += playerWord.points
-			} 
-		}
-		player.pointsPerDay.put(day, totalForDay)
-	}
-}
-println "calcul des points pour les joueurs ok"
+computePointsForPlayers(players, pointsForWordsOfDays)
+println "> calcul des points pour les joueurs ok"
 
 // affichage
-// TODO : génration output via template
-pointsForWordsOfDays.each { day, words ->
-	println "pour le jour $day les mots sont $words"
-}
-
-players.each { player ->
-	//println player
-	player.pointsPerDay.each { day, points ->
-		println "Pour le jour $day le joueur $player.name a $points points"
-	}
-	println "le joueur $player.name a $player.totalPoints points au total"
-}
+def outTemplateText = new File('.', 'resultats.template').text
+def engine = new groovy.text.SimpleTemplateEngine()
+def template = engine.createTemplate(outTemplateText).make(["maxDay": maxDay, "pointsForWordsOfDays": pointsForWordsOfDays, "players": players.sort {a,b -> b.totalPoints <=> a.totalPoints}])
+def output = new File('.', 'output.html')
+output.newWriter().withWriter { it << template.toString() }
 
 /**************************************************************************************************************
 *                                                  METHODES ET CLASSES
@@ -81,7 +65,38 @@ List<Word> computePointsForWordsOfDay(int day, List<Player> players) {
 		}
 	}
 	
-	wordsForDay
+	wordsForDay.sort { a,b -> b.points <=> a.points }
+}
+
+def computePointsForPlayers(List<Player> players, Map pointsForWordsOfDays){
+	players.each { player ->
+		player.proposalsPerDay.each { day, playerWords ->
+			def wordsOfDay = pointsForWordsOfDays[day]
+			def totalForDay = 0
+			playerWords.each { playerWord ->
+				def wordOfDay = wordsOfDay.find { it.value == playerWord.value }
+				if(wordOfDay){
+					playerWord.points  = wordOfDay.points
+					totalForDay += playerWord.points
+				} 
+			}
+			player.pointsPerDay.put(day, totalForDay)
+		}
+	}
+}
+
+def checkInput(List<Player> players){
+	println "> vérification des données"
+	players.each { player ->
+		player.proposalsPerDay.each { day, words ->
+			if(words.toSet().size() != words.size()){
+				println "/!\\ WARN : le joueur $player.name a des doublons pour le jour $day"
+			}
+			if(words.size() != 8) {
+				println "/!\\ WARN : le joueur $player.name a moins de 8 mots pour le jour $day"	
+			}
+		}
+	}
 }
 
 @ToString
@@ -99,4 +114,12 @@ class Player {
 class Word {
 	String value
 	Integer points = 0
+
+	boolean equals(Object o){
+		return o.value == this.value
+	}
+
+	int hashCode(){
+		return value.hashCode()
+	}
 }
