ee59d0ba58f93f4f888145d247fd816748c335cb
FRANCK <david.franck@cbp-prevoyance.net>
2017-02-22 13:24:10 +0100

    mise à jour de la verification

--- version_groovy/v2/unanimo.groovy
+++ version_groovy/v2/unanimo.groovy
@@ -19,31 +19,34 @@ inputData.days.each { day ->
 	day.players.each { inputPlayer ->
 		def currentPlayer = players.find { it.name == inputPlayer.name }
 		if(!currentPlayer) {
-			println "creation du joueur $inputPlayer.name"
+			//println "creation du joueur $inputPlayer.name"
 			currentPlayer = new Player(name: inputPlayer.name)
 			players << currentPlayer
 		}
-		println "on traite le joueur $inputPlayer.name / propositions : $inputPlayer.proposals"
+		//println "on traite le joueur $inputPlayer.name / propositions : $inputPlayer.proposals"
 		def words = inputPlayer.proposal.collect { word ->
 			word = formatWord(word)
 			def currentWord = allWordsOfADay.find { it.value == word }
 			if(!currentWord){
-				println "ajout du mot $word"
+				//println "ajout du mot $word"
 				currentWord = new Word(value: formatWord(word)) 
 				allWordsOfADay << currentWord
 			} else {
-				println "le mot $word existe déjà, on incrémente les points"
+				//println "le mot $word existe déjà, on incrémente les points"
 				currentWord.points = currentWord.points == 0 ? 2 : currentWord.points + 1
 			}
 			currentWord
 		}
-		currentPlayer.proposals << new Proposal(day: day.index, words:words)
+		currentPlayer.proposals << new Proposal(day: day.index, words:words, player: currentPlayer)
 	}
 	wordsPerDay.put(day.index, allWordsOfADay)
 }
 
-println players
-println wordsPerDay
+players.each { player ->
+	player.proposals.each {
+		it.check()
+	}
+}
 
 // affichage
 def outTemplateText = new File('.', 'resultats.template').text
@@ -66,8 +69,18 @@ class Player {
 class Proposal {
 	int day
 	List<Word> words
+	Player player
 
 	int getPoints() { words.inject(0) { acc, word -> acc + word.points } }
+
+	boolean check() {
+		if(words.size() != 8) {
+			println "nombre de mots invalides pour la proposition du jour $day du joueur $player.name"
+		}
+		if(words.size() != words.toSet().size()){
+			println "présence de doublons dans la proposition du jour $day du joueur $player.name"
+		}
+	}
 }
 
 @ToString
