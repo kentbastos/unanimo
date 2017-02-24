b304fe54e35c9531be734ffc8be574f6bef2cca3
FRANCK <david.franck@cbp-prevoyance.net>
2017-02-24 11:50:18 +0100

    correction template foireux

--- version_groovy/v2/inputFormater.groovy
+++ version_groovy/v2/inputFormater.groovy
@@ -1,11 +1,10 @@
-def allPlayersWordsForADay = """23/02/2017 09:01:31,FEU,MARTEAU,TAPIS,VIN,COULEUR,SANG,STOP,ROSE,flavien.barthe@cbp-group.com
-23/02/2017 09:14:41,sang,marteau,vif,feu,communiste,russe,vin,viande,cedric.linez@cbp-group.com
-23/02/2017 09:20:15,sang,bleu,blanc,tomate,poivron,colère,noir,ferrari,heloise.duplouy@cbp-group.com
-23/02/2017 09:28:57,NOIR,BLEU,SANG,POURPRE,SOCIALISTE,VIF,RAGE,COULEUR,catalin.costea@cbp-group.com
-23/02/2017 09:55:41,TAUREAU,SANG,VIN,URSS,POISSON,NEZ,CHINE,VIANDE,quentin.moquay@cbp-group.com
-23/02/2017 10:11:38,BLEU,VERT,STOP,SANG,FRAISE,CERISE,PRIMAIRE,COULEUR,eric.vepa@cbp-group.com
-23/02/2017 11:17:58,Camion,Pompiers,Sang,Ecarlate,Cerise,Communisme,Colère,Pomme,matthias.david@cbp-group.com
-23/02/2017 11:50:26,peau,colère,sang,couleur,crayon,peinture,ferrari,voiture,nicolas.jajolet@cbp-group.com"""
+def allPlayersWordsForADay = """24/02/2017 09:52:18,slip,animal,australie,saut,box,poche,steak,bush,cedric.linez@cbp-group.com
+24/02/2017 09:52:33,Australie,Poche,Saut,boxe,walibi,aborigène,sac,animal,heloise.duplouy@cbp-group.com
+24/02/2017 09:54:35,AUSTRALIE,POCHE,SAUT,BOXE,MARSUPIAL,DESERT,SKIPPY,WALLABY,flavien.barthe@cbp-group.com
+24/02/2017 10:05:05,AUSTRALIE,BOOMERANG,STEAK,SAUT,BOXE,ANIMAL,POCHE,BOND,eric.vepa@cbp-group.com
+24/02/2017 10:16:28,Skippy,Australie,Boxe,Saut,Viande,Poche,Bonds,Vitesse,matthias.david@cbp-group.com
+24/02/2017 10:20:40,AUSTRALIE,POCHE,SAUT,MARSUPIAL,BOXE,WALLABY,ANIMAL,BEBE,catalin.costea@cbp-group.com
+24/02/2017 10:59:44,AUSTRALIE,BOXE,POCHE,ANIMAL,SAUT,WALIBI,MARSUPIAL,BOND,quentin.moquay@cbp-group.com"""
 
 allPlayersWordsForADay.eachLine { line ->
 	def tokens = line.tokenize(',')
